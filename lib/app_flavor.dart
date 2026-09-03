d6510800fc2937b8a298ec3e81a00cfc3bacd089
Gautam <159130133+CookieCums@users.noreply.github.com>
2026-09-04 04:31:29 +0530

    Remove obsolete Play flavor from app model

--- lib/app_flavor.dart
+++ lib/app_flavor.dart
@@ -1,23 +1,7 @@
-enum AppFlavor { play, izzy, libre }
+enum AppFlavor { izzy, libre }
 
 extension ExtraAppFlavor on AppFlavor {
-  bool get canEnableErrorReporting {
-    switch (this) {
-      case .play:
-        return true;
-      case .izzy:
-      case .libre:
-        return false;
-    }
-  }
+  bool get canEnableErrorReporting => false;
 
-  bool get hasMapStyleDefault {
-    switch (this) {
-      case .play:
-        return true;
-      case .izzy:
-      case .libre:
-        return false;
-    }
-  }
+  bool get hasMapStyleDefault => false;
 }
