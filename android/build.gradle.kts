385e3ec17ce0601498025a8bf4dcd2ca9560abd8
Gautam <159130133+CookieCums@users.noreply.github.com>
2026-09-04 04:23:01 +0530

    Remove Firebase Crashlytics Gradle setup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,17 +1,8 @@
 buildscript {
-    extra["aves_useCrashlytics"] = gradle.startParameter.taskNames.any { it.contains("play", ignoreCase = true) }
+    extra["aves_useCrashlytics"] = false
 
     println("Tasks=${gradle.startParameter.taskNames}")
     println("Extra=\n${extra.properties.entries.map { kv -> "  ${kv.key}=${kv.value}" }.sorted().joinToString("\n")}")
-
-    // conditional dependencies cannot be moved to the static `plugins` block
-    if (rootProject.extra["aves_useCrashlytics"] as Boolean) {
-        dependencies {
-            // GMS & Firebase Crashlytics (used by some flavors only)
-            classpath(libs.google.gms)
-            classpath(libs.google.firebase.crashlytics)
-        }
-    }
 }
 
 plugins {
